88ab9578af1aae4f4179b007097d99e2268b33dd
ReneRambow <bill12gate@gmail.com>
2024-02-26 13:14:02 +0100

    shuffle

---
+++ resources/kaarten1/script.js
@@ -0,0 +1,34 @@
+let colors = ['color1', 'color2','color3','color4'];
+let numbers = ['nr0','nr1','nr1','nr2','nr2','nr3','nr3','nr4','nr4','nr5','nr5','nr6','nr6','nr7','nr7','nr8','nr8', 'nr9','nr9','joker', 'plus4']
+
+
+function build_card(card_color, card_number) {
+    return '<div class="card '+card_color+' '+card_number+'"></div>'
+}
+
+function shuffle(array) {
+    array.sort((a, b) => 0.5 - Math.random());
+}
+
+
+let deck = [];
+
+for (let index = 0; index < colors.length; index++) {
+    const color = colors[index];
+
+
+
+    for (let index = 0; index < numbers.length; index++) {
+        const number = numbers[index];
+            
+        const card = build_card(color, number);
+    
+        deck.push(card);
+    }
+}
+
+shuffle(deck)
+
+for (let index = 0; index < deck.length; index++) {
+    carddeck.innerHTML += deck[index];
+}
