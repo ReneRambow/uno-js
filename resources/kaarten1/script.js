8c1121a8cb208af9f187608c61bb9a93a9f3a593
ReneRambow <bill12gate@gmail.com>
2024-02-29 09:57:40 +0100

    Grab card

--- resources/kaarten1/script.js
+++ resources/kaarten1/script.js
@@ -1,5 +1,5 @@
 let colors = ['color1', 'color2','color3','color4'];
-let numbers = ['nr0','nr1','nr1','nr2','nr2','nr3','nr3','nr4','nr4','nr5','nr5','nr6','nr6','nr7','nr7','nr8','nr8', 'nr9','nr9','joker', 'plus4']
+let numbers = ['nr0','nr1','nr1','nr2','nr2','nr3','nr3','nr4','nr4','nr5','nr5','nr6','nr6','nr7','nr7','nr8','nr8', 'nr9','nr9','skip', 'skip', 'reverse', 'reverse', 'plus2', 'plus2','joker', 'plus4']
 
 
 function build_card(card_color, card_number) {
@@ -27,8 +27,18 @@ for (let index = 0; index < colors.length; index++) {
     }
 }
 
-shuffle(deck)
+function grab_card(){
+    if (deck.length > 0 ){
+        shuffle(deck)
+        const Deck = deck.pop()
+        carddeck.innerHTML += Deck;
+    }
+    
+    else{
+        alert("De kaarten zijn op")
+        this.remove()
+    }
 
-for (let index = 0; index < deck.length; index++) {
-    carddeck.innerHTML += deck[index];
 }
+
+grab.addEventListener('click', grab_card);
